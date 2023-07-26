6278aa4b3c64d0fce8a46c09c3d8f16100fbfbe9
Ovy Evbodi <96619760+OvyEvbodi@users.noreply.github.com>
2023-07-27 00:05:54 +0100

    Update 1-fifo_cache.py

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -16,13 +16,13 @@ class FIFOCache(BaseCaching):
         """
         if key and item:
             self.cache_data[key] = item
-            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
                 discarded_item = next(iter(self.cache_data))
                 del self.cache_data[discarded_item]
-                print('ddISCARD: {}'.format(discarded_item))
+                print('DISCARD: {}'.format(discarded_item))
 
     def get(self, key):
         """ Get an item by key
         """
-        return self.cache_data.get(key) if key and key in self.cache_data else None
+        return self.cache_data.get(key)
         
