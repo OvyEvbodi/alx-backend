7df20e87ebf451d219cb066333d552048a087228
Ovy Evbodi <evbodiovo@gmail.com>
2023-07-26 20:15:22 +0100

    Make scripts executable

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -1,25 +1,22 @@
 #!/usr/bin/env python3
 
-""" Inherits from BaseCaching and is a FIFO caching system
+""" Inherits from BaseCaching and is a caching system
 """
 
 BaseCaching = __import__('base_caching').BaseCaching
 
-class FIFOCache(BaseCaching):
+class BasicCache(BaseCaching):
     """ BaseCaching defines:
       - constants of your caching system
       - where your data are stored (in a dictionary)
     """
-
+    
+    
     def put(self, key, item):
         """ Add an item in the cache
         """
         if key and item:
             self.cache_data[key] = item
-            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-                discarded_item = next(iter(self.cache_data))
-                del self.cache_data[discarded_item]
-                print('ddISCARD: {}'.format(discarded_item))
 
     def get(self, key):
         """ Get an item by key
