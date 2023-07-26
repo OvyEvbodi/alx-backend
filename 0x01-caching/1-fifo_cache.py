45bd043031167387ae5ff23a05f1a7732ebafa13
Ovy Evbodi <evbodiovo@gmail.com>
2023-07-26 20:10:47 +0100

    fifo cache policy

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+""" Inherits from BaseCaching and is a FIFO caching system
+"""
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+class FIFOCache(BaseCaching):
+    """ BaseCaching defines:
+      - constants of your caching system
+      - where your data are stored (in a dictionary)
+    """
+
+    def put(self, key, item):
+        """ Add an item in the cache
+        """
+        if key and item:
+            self.cache_data[key] = item
+            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+                discarded_item = next(iter(self.cache_data))
+                del self.cache_data[discarded_item]
+                print('ddISCARD: {}'.format(discarded_item))
+
+    def get(self, key):
+        """ Get an item by key
+        """
+        return self.cache_data.get(key) if key and key in self.cache_data else None
+        
